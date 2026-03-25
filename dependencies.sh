fba984fa4db36e3fa683cf182c94a34363823349
Sahil Gautam <printfdebugging@gmail.com>
2026-03-26 04:45:52 +0530

    some bash script corrections and add ffmpeg build dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -13,7 +13,7 @@ if [ -f "/etc/os-release" ]; then
 	case "$ID" in
 	'msys2')
 		{
-            pacman -Sy
+			pacman -Sy \
 				'clang64/mingw-w64-clang-x86_64-gdb' \
 				'clang64/mingw-w64-clang-x86_64-cmake' \
 				'clang64/mingw-w64-clang-x86_64-ninja' \
@@ -26,28 +26,32 @@ if [ -f "/etc/os-release" ]; then
 				'clang64/mingw-w64-clang-x86_64-make' \
 				'clang64/mingw-w64-clang-x86_64-ripgrep' \
 				'clang64/mingw-w64-clang-x86_64-nasm' \
+				'clang64/mingw-w64-clang-x86_64-cc' \
 				'clang64/mingw-w64-clang-x86_64-diffutils' \
 				'clang64/mingw-w64-clang-x86_64-meson' \
 				'clang64/mingw-w64-clang-x86_64-neovim' \
 				'clang64/mingw-w64-clang-x86_64-rust' \
 				'clang64/mingw-w64-clang-x86_64-git' \
-				'clang64/mingw-w64-clang-x86_64-pkg-config' \
+				'clang64/mingw-w64-clang-x86_64-pkgconf' \
 				'clang64/mingw-w64-clang-x86_64-vulkan-headers' \
-				'clang64/mingw-w64-clang-x86_64-vulkan-loader'
+				'clang64/mingw-w64-clang-x86_64-vulkan-loader' \
+				'clang64/mingw-w64-clang-x86_64-amf-headers' \
+				'clang64/mingw-w64-clang-x86_64-autotools' \
+				'clang64/mingw-w64-clang-x86_64-dlfcn' \
+				'clang64/mingw-w64-clang-x86_64-ffnvcodec-headers'
 
-			install_bear_on_windows
+			# install_bear_on_windows
 		}
 		;;
 	'arch')
 		{
-            sudo pacman -S --noconfirm \
+			sudo pacman -S --noconfirm \
 				'cmake' \
 				'make' \
 				'clang' \
 				'gdb' \
 				'ccache' \
-				'vulkan-devel' \
-			)
+				'vulkan-devel'
 		}
 		;;
 	*) echo "distribution not supported" && exit 1 ;;
