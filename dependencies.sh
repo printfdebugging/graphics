05e9c17cc23989faced9c2c14b21831b0570c4d0
Sahil Gautam <printfdebugging@gmail.com>
2026-03-25 20:21:30 +0530

    add partial bear build steps to dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/env bash
 
+function install_bear_on_windows() {
+	bear_upstream=https://github.com/rizsotto/Bear
+	bear_source="$HOME/Bear"
+	[ ! -d "${bear_source}" ] && git clone "${bear_upstream}" "${bear_source}"
+	(cd "${bear_source}" && cargo build --release)
+}
+
 if [ -f "/etc/os-release" ]; then
 	. /etc/os-release
 	case "$ID" in
@@ -23,6 +30,7 @@ if [ -f "/etc/os-release" ]; then
 				'meson'
 				'neovim'
 				'rust'
+				'git'
 				'vulkan-headers'
 				'vulkan-loader'
 			)
@@ -30,6 +38,8 @@ if [ -f "/etc/os-release" ]; then
 			for package in "${packages[@]}"; do
 				pacman -Sy "${prefix}-${package}"
 			done
+
+			install_bear_on_windows
 		}
 		;;
 	'arch')
