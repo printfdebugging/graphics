ea153545a438b7df6014fec806d007665469919a
Sahil Gautam <printfdebugging@gmail.com>
2026-03-27 22:27:36 +0530

    cleanup build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ pkgconfig_path="$(pwd)/install/lib/pkgconfig"
 application_source_dir="$(pwd)"
 application_build_dir="$(pwd)/build"
 
-cmake \
-	-DPKG_CONFIG_PATH="${pkgconfig_path}" \
-	-S "${application_source_dir}" \
-	-B "${application_build_dir}"
-
+export PKG_CONFIG_PATH="${pkgconfig_path}"
+cmake -S "${application_source_dir}" -B "${application_build_dir}"
 cmake --build "${application_build_dir}"
