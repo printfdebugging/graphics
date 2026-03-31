1afda5782dbef8da325e75c2f51d825e4019f2b8
Sahil Gautam <printfdebugging@gmail.com>
2026-03-31 14:55:56 +0530

    add emtpy model.h and a gltf cube model

---
+++ include/model.h
@@ -0,0 +1,9 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+struct model {
+};
+
+struct model *load_model();
+
+#endif
---
+++ source/model.c
@@ -0,0 +1,6 @@
+#include "model.h"
+#include <cgltf/cgltf.h>
+
+struct model *load_model() {
+    return NULL;
+}
