37eced854f155f9200e7119f32e231685e13c979
Sahil Gautam <printfdebugging@gmail.com>
2026-05-23 06:15:11 +0530

    shrink the cube to unit dimensions

--- include/game.h
+++ include/game.h
@@ -4,6 +4,10 @@
 #include "window.h"
 #include "camera.h"
 
+/* this is the game data which is passed around in the callbacks. this is  not
+ * a place to store textures etc, they live separately, though they should be
+ * accessible from all other subsystems wherever they are needed, or maybe in the
+ * game code, who knows we might do some cool things with them. */
 struct GameData {
         struct Window *window;
         struct Camera *camera;
--- source/main.c
+++ source/main.c
@@ -75,7 +75,7 @@ int main() {
                 {
                         /* render model*/
                         cube->model      = (mat4s) { .raw = GLM_MAT4_IDENTITY_INIT };
-                        cube->model      = glms_scale(cube->model, (vec3s) { 2.0f, 16.0f, 16.0f });
+                        cube->model      = glms_scale(cube->model, (vec3s) { 1.0f, 1.0f, 1.0f });
                         cube->view       = view;
                         cube->projection = projection;
                         glUseProgram(cubeShader->program);
