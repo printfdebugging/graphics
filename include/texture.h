2323ef8d7a034fb8b8a14b4a3731209e9b4871a8
Sahil Gautam <printfdebugging@gmail.com>
2026-05-23 17:51:41 +0530

    remove unnecessary TextureIndex enumerator
    it's easy to confuse GL_TEXTURE0 with TEXTURE0 when clearly they are
    very different things, TextureIndex is just numbers and we can assign
    them manually well enough.

--- include/texture.h
+++ include/texture.h
@@ -3,42 +3,6 @@
 
 #include "glad/glad.h"
 
-enum TextureIndex {
-   TEXTURE0 = 0,
-   TEXTURE1 = 1,
-   TEXTURE2 = 2,
-   TEXTURE3 = 3,
-   TEXTURE4 = 4,
-   TEXTURE5 = 5,
-   TEXTURE6 = 6,
-   TEXTURE7 = 7,
-   TEXTURE8 = 8,
-   TEXTURE9 = 9,
-   TEXTURE10 = 10,
-   TEXTURE11 = 11,
-   TEXTURE12 = 12,
-   TEXTURE13 = 13,
-   TEXTURE14 = 14,
-   TEXTURE15 = 15,
-   TEXTURE16 = 16,
-   TEXTURE17 = 17,
-   TEXTURE18 = 18,
-   TEXTURE19 = 19,
-   TEXTURE20 = 20,
-   TEXTURE21 = 21,
-   TEXTURE22 = 22,
-   TEXTURE23 = 23,
-   TEXTURE24 = 24,
-   TEXTURE25 = 25,
-   TEXTURE26 = 26,
-   TEXTURE27 = 27,
-   TEXTURE28 = 28,
-   TEXTURE29 = 29,
-   TEXTURE30 = 30,
-   TEXTURE31 = 31,
-   TEXTURE_COUNT = 32,
-};
-
 struct texture {
    unsigned int texture;
    short int texture_index;
