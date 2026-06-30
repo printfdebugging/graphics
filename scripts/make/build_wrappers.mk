4f21d33497e6137dca25505c8c29cb02c3ecbbc4
Sahil Gautam <printfdebugging@gmail.com>
2026-06-30 12:26:00 +0530

    build: part-03: separate configure & build steps
    Signed-off-by: Sahil Gautam <printfdebugging@gmail.com>

--- scripts/make/build_wrappers.mk
+++ scripts/make/build_wrappers.mk
@@ -1,7 +1,7 @@
 define build_wrapper_cmake
 $(1)_CONFIGURE_FLAGS=$$(shell cat $$(EXTERNALS_CONFIG)/$(1)/{flags.common,flags.$$(PLATFORM_CONFIG_EXTENSION)} | xargs)
 
-$(1).build:
+$(1).configure:
 	cmake \
 		-DCMAKE_BUILD_TYPE=$$(CMAKE_BUILD_TYPE) \
 		-DCMAKE_INSTALL_PREFIX=$$(INSTALL_DIRECTORY) \
@@ -10,50 +10,52 @@ $(1).build:
 		-B "$$(BUILD_DIRECTORY)/$(1)" \
 		-S "$$(EXTERNALS)/$(1)"
 
+$(1).build:
 	cmake --build "$$(BUILD_DIRECTORY)/$(1)"
-
-$(1).clean:
-	rm -rf "$$(BUILD_DIRECTORY)/$(1)"
 
 $(1).install:
 	cmake --install "$$(BUILD_DIRECTORY)/$(1)" --prefix "$$(INSTALL_DIRECTORY)"
+
+$(1).clean:
+	rm -rf "$$(BUILD_DIRECTORY)/$(1)"
 endef
 
 define build_wrapper_meson
 $(1)_CONFIGURE_FLAGS=$$(shell cat $$(EXTERNALS_CONFIG)/$(1)/{flags.common,flags.$$(PLATFORM_CONFIG_EXTENSION)} | xargs)
 
-$(1).build:
+$(1).configure:
 	meson setup \
 		-Dbuildtype=$$(MESON_BUILD_TYPE) \
 		$$($(1)_CONFIGURE_FLAGS) \
 		--prefix=$$(INSTALL_DIRECTORY) \
 		"$$(BUILD_DIRECTORY)/$(1)" "$$(EXTERNALS)/$(1)"
 
+$(1).build:
 	meson compile -C "$$(BUILD_DIRECTORY)/$(1)"
-
-$(1).clean:
-	rm -rf "$$(BUILD_DIRECTORY)/$(1)"
 
 $(1).install:
 	meson install -C "$$(BUILD_DIRECTORY)/$(1)"
+
+$(1).clean:
+	rm -rf "$$(BUILD_DIRECTORY)/$(1)"
 endef
 
 define build_wrapper_autotools
 $(1)_CONFIGURE_FLAGS=$$(shell cat $$(EXTERNALS_CONFIG)/$(1)/{flags.common,flags.$$(PLATFORM_CONFIG_EXTENSION)} | xargs)
 
-$(1).build:
+$(1).configure:
 	mkdir -p "$$(BUILD_DIRECTORY)/$(1)"
 	cd "$$(BUILD_DIRECTORY)/$(1)" && \
 	"$$(EXTERNALS)/$(1)/configure" \
 		$$($(1)_CONFIGURE_FLAGS) \
-		--prefix=$$(INSTALL_DIRECTORY) \
-	
+		--prefix=$$(INSTALL_DIRECTORY)
+
+$(1).build:
 	make -C "$$(BUILD_DIRECTORY)/$(1)" -j
-
-
-$(1).clean:
-	rm -rf "$$(BUILD_DIRECTORY)/$(1)"
 
 $(1).install:
 	make -C "$$(BUILD_DIRECTORY)/$(1)" install
+
+$(1).clean:
+	rm -rf "$$(BUILD_DIRECTORY)/$(1)"
 endef
