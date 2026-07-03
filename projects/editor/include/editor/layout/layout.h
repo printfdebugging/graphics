fd114cb08d999084428de65483852c3fd7c3afa1
Sahil Gautam <printfdebugging@gmail.com>
2026-07-03 22:33:18 +0530

    chore: add a note about making row operations a buffer specific thing
    Signed-off-by: Sahil Gautam <printfdebugging@gmail.com>

--- projects/editor/include/editor/layout/layout.h
+++ projects/editor/include/editor/layout/layout.h
@@ -3,8 +3,15 @@
 
 #include "editor/editor.h"
 
-/* this should later be changed to buffer specific operation as
- * a buffer can have smaller/larger font size than another buffer.*/
+/* note: this should later be changed to buffer specific operation as
+ * a buffer can have smaller/larger font size than another buffer.
+ *
+ * since this would be changed to be a buffer specific thing, it
+ * should happen after the buffer data has been loaded and all the
+ * formatting calculations are taken into account. like a row can be
+ * larger than other rows. emacs i think does this, one row with a
+ * constant height, text quads can be smaller/larger in that.
+ */
 status editor_count_rows(struct editor_state *editor);
 
 #endif
